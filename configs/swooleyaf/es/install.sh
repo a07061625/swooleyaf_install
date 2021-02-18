153f20c656970c3fb799f4362f0aa59cd4e9470a
jiangwei <jiangwei07061625@163.com>
2021-02-18 11:40:37 +0800

    fixed error for elasticsearch

--- configs/swooleyaf/es/install.sh
+++ configs/swooleyaf/es/install.sh
@@ -13,6 +13,19 @@ vim /etc/security/limits.conf
     root soft memlock unlimited
     root hard memlock unlimited
 
+cd /usr/local/elasticsearch
+# 插件
+## 在线安装
+bin/elasticsearch-plugin install https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v7.7.1/elasticsearch-analysis-ik-7.7.1.zip
+bin/elasticsearch-plugin install https://github.com/medcl/elasticsearch-analysis-pinyin/releases/download/v7.7.1/elasticsearch-analysis-pinyin-7.7.1.zip
+
+## 压缩包安装
+cd /usr/local/elasticsearch/plugins
+unzip analysis-ik-v7.7.1.zip
+unzip analysis-pinyin-v7.7.1.zip
+rm -rf analysis-ik-v7.7.1.zip
+rm -rf analysis-pinyin-v7.7.1.zip
+
 mkdir /home/data/elasticsearch
 mkdir /home/logs/elasticsearch
 chown -R www:www /usr/local/elasticsearch
@@ -20,22 +33,6 @@ chown -R www:www /home/logs/elasticsearch
 chown -R www:www /home/data/elasticsearch
 
 cd /usr/local/elasticsearch
-# 插件-在线安装
-bin/elasticsearch-plugin install https://github.com/medcl/elasticsearch-analysis-ik/releases/download/v7.7.1/elasticsearch-analysis-ik-7.7.1.zip
-bin/elasticsearch-plugin install https://github.com/medcl/elasticsearch-analysis-pinyin/releases/download/v7.7.1/elasticsearch-analysis-pinyin-7.7.1.zip
-
-# 插件-压缩包安装
-cd /usr/local/elasticsearch/plugins
-unzip analysis-ik-v7.7.1.zip
-unzip analysis-pinyin-v7.7.1.zip
-rm -rf analysis-ik-v7.7.1.zip
-rm -rf analysis-pinyin-v7.7.1.zip
-
-cd /usr/local/elasticsearch
-# 启动
-bin/elasticsearch
-# 后台启动
-bin/elasticsearch -d
 # 证书生成
 bin/elasticsearch-certutil ca //输入自定义密码
 bin/elasticsearch-certutil cert --ca elastic-stack-ca.p12 //输入自定义密码
@@ -45,6 +42,10 @@ mv elastic-certificates.p12 config/certs/
 bin/elasticsearch-keystore add xpack.security.transport.ssl.keystore.secure_password //密码为创建证书时设置的密码
 bin/elasticsearch-keystore add xpack.security.transport.ssl.truststore.secure_password //密码为创建证书时设置的密码
 
+# 启动
+bin/elasticsearch
+# 后台启动
+bin/elasticsearch -d
 # 初始化密码
 bin/elasticsearch-setup-passwords interactive
 # 清除日志
--- configs/swooleyaf/logs/kibana/install.sh
+++ configs/swooleyaf/logs/kibana/install.sh
@@ -2,6 +2,9 @@
 set -o nounset
 set -o errexit
 
+mkdir /home/logs/kibana
+touch /home/logs/kibana/kibana.pid
+touch /home/logs/kibana/kibana.log
 cd /usr/local/kibana
 # 启动
 bin/kibana --allow-root
