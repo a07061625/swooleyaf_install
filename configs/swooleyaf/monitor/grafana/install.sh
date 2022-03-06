7c78926b34e2be41e152e16ca5b2ab9fc921f5d6
jiangwei <jiangwei07061625@163.com>
2022-03-07 00:22:56 +0800

    feat(monitor): add prometheus and grafana

--- configs/swooleyaf/monitor/grafana/install.sh
+++ configs/swooleyaf/monitor/grafana/install.sh
@@ -3,10 +3,15 @@ set -o nounset
 set -o errexit
 
 # dashboard
-## mongodb_exporter: 7353
-## node_exporter: 9276
-## mysqld_exporter: 7362
-## redis_exporter: 763
-## nginx-vts-exporter: 2949
-## telegraf system: 928
-## telegraf metrics: 61
+## influxdb1
+### mongodb_exporter: 7353
+### node_exporter: 9276
+### mysqld_exporter: 7362
+### redis_exporter: 763
+### nginx-vts-exporter: 2949
+### telegraf metrics: 61
+### telegraf nginx-vts: 11967
+### telegraf netdata: 12183
+
+## influxdb2
+### telegraf system: 15650
