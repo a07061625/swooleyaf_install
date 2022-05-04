fa6dc4297ce02a16c81f54b07ab51ecb88e72349
jiangwei <jiangwei07061625@163.com>
2022-05-04 15:03:19 +0800

    feat(shell): add notify wx

--- configs/swooleyaf/shell/collects/scripts/libs_constants_common.sh
+++ configs/swooleyaf/shell/collects/scripts/libs_constants_common.sh
@@ -7,3 +7,9 @@ YXP_COMMON_CLI_OPTION_TITLE1="帮助"
 YXP_COMMON_DINGDING_URL="https://oapi.dingtalk.com/robot/send?access_token=xxx"
 YXP_COMMON_DINGDING_PREFIX='{"msgtype":"text", "at":{"atMobiles":[], "isAtAll": false}, "text":{"content":"YXP告警'
 YXP_COMMON_DINGDING_SUFFIX='"}}'
+# 企业号ID
+YXP_COMMON_WX_CORP_ID="xxx"
+# 企业密钥
+YXP_COMMON_WX_CORP_SECRET="xxx"
+# 企业应用ID
+YXP_COMMON_WX_APP_ID="xxx"
--- configs/swooleyaf/shell/collects/scripts/libs_methods_common.sh
+++ configs/swooleyaf/shell/collects/scripts/libs_methods_common.sh
@@ -78,3 +78,63 @@ function yxpCommonNotifyDingTalk() {
     YXP_COMMON_NOTIFY_RES=`curl "${YXP_COMMON_DINGDING_URL}" -H 'Content-Type:application/json;charset=utf-8' -X POST -d @${YXP_COMMON_NOTIFY_MSG_FILE}`
     echo -e "\e[1;36m钉钉通知: \e[0m \e[1;32m \t[完成发送消息,发送结果: ${YXP_COMMON_NOTIFY_RES}] \e[0m"
 }
+
+function yxpCommonNotifyWxToken() {
+    echo -e "\e[1;36m企业微信通知: \e[0m \e[1;35m \t[获取微信令牌开始] \e[0m"
+    YXP_COMMON_WX_TOKEN_FILE=${YXP_COMMON_PATH_TEMP}/wx_token.txt
+    # shellcheck disable=SC2086
+    if [ -f ${YXP_COMMON_WX_TOKEN_FILE} ]; then
+        # shellcheck disable=SC2006
+        # shellcheck disable=SC2002
+        YXP_COMMON_WX_TIME_EXPIRE=`cat ${YXP_COMMON_WX_TOKEN_FILE} | awk '{print $1;}'`
+    else
+        YXP_COMMON_WX_TIME_EXPIRE=0
+    fi
+
+    # shellcheck disable=SC2006
+    YXP_COMMON_WX_TIME_NOW=`date +%s`
+    # shellcheck disable=SC2086
+    if [ ${YXP_COMMON_WX_TIME_EXPIRE} -ge ${YXP_COMMON_WX_TIME_NOW} ]; then
+        # shellcheck disable=SC2006
+        # shellcheck disable=SC2002
+        YXP_COMMON_WX_TOKEN=`cat ${YXP_COMMON_WX_TOKEN_FILE} | awk '{print $2;}'`
+    else
+        # shellcheck disable=SC2006
+        YXP_COMMON_WX_TOKEN=`curl -s -G "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=${YXP_COMMON_WX_CORP_ID}&corpsecret=${YXP_COMMON_WX_CORP_SECRET}" | awk -F \" '{print $10}'`
+        if [ ${#YXP_COMMON_WX_TOKEN} -gt 10 ]; then
+            # shellcheck disable=SC2004
+            echo "$((${YXP_COMMON_WX_TIME_NOW}+300)) ${YXP_COMMON_WX_TOKEN}" > ${YXP_COMMON_WX_TOKEN_FILE}
+        else
+            YXP_COMMON_WX_TOKEN=""
+        fi
+    fi
+    if [ "${YXP_COMMON_WX_TOKEN}aaa" != "aaa" ]; then
+        echo -e "\e[1;36m企业微信通知: \e[0m \e[1;32m \t[获取微信令牌成功] \e[0m"
+    else
+        echo -e "\e[1;36m企业微信通知: \e[0m \e[1;31m \t[获取微信令牌失败] \e[0m"
+    fi
+}
+
+function yxpCommonNotifyWx() {
+    yxpCommonNotifyWxToken
+    if [ "${YXP_COMMON_WX_TOKEN}aaa" == "aaa" ]; then
+        return
+    fi
+
+    echo -e "\e[1;36m企业微信通知: \e[0m \e[1;35m \t[开始发送消息] \e[0m"
+    YXP_COMMON_WX_MSG='{"touser":"'
+    YXP_COMMON_WX_MSG=${YXP_COMMON_WX_MSG}${YXP_COMMON_WX_USER_ID}
+    YXP_COMMON_WX_MSG=${YXP_COMMON_WX_MSG}'","toparty":"'
+    YXP_COMMON_WX_MSG=${YXP_COMMON_WX_MSG}${YXP_COMMON_WX_PARTY_ID}
+    YXP_COMMON_WX_MSG=${YXP_COMMON_WX_MSG}'","agentid":"'
+    YXP_COMMON_WX_MSG=${YXP_COMMON_WX_MSG}${YXP_COMMON_WX_APP_ID}
+    YXP_COMMON_WX_MSG=${YXP_COMMON_WX_MSG}'","safe":"0","msgtype":"text","text":{"content":"YXP告警'
+    YXP_COMMON_WX_MSG=${YXP_COMMON_WX_MSG}${YXP_COMMON_WX_MSG_CONTENT}
+    YXP_COMMON_WX_MSG=${YXP_COMMON_WX_MSG}'"}}'
+    # shellcheck disable=SC2086
+    echo "${YXP_COMMON_WX_MSG}" > ${YXP_COMMON_NOTIFY_MSG_FILE}
+    # shellcheck disable=SC2086
+    # shellcheck disable=SC2006
+    YXP_COMMON_NOTIFY_RES=`curl "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=${YXP_COMMON_WX_TOKEN}" -H 'Content-Type:application/json;charset=utf-8' -X POST -d @${YXP_COMMON_NOTIFY_MSG_FILE}`
+    echo -e "\e[1;36m企业微信通知: \e[0m \e[1;32m \t[完成发送消息,发送结果: ${YXP_COMMON_NOTIFY_RES}] \e[0m"
+}
